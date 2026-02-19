a64da8a12d24e7e77a9c745433bd1ae00bc68007
osaygin <oktaysaygin17@gmail.com>
2026-02-19 09:12:35 +0300

    webView deneme

--- routes/webView.js
+++ routes/webView.js
@@ -16,17 +16,17 @@ router.get('/', (req, res) => {
     <section data-type="ramadanWidget" class="mb-4 w-full flex col-span-12 row-span-1 flex-col px-2">
         <div class="w-full ramadan-counter">
             <div class="w-full text-3xl text-white text-center">
-                <a href="https://adsp.haberturk.com/advertpro/servlet/click/media?mid=25550&pid=0&pixel=true&random=[random]" rel="nofollow" target="_blank" class="relative flex items-center justify-center h-12">
+                <a href="cbg://ramazan/anasayfa" rel="nofollow" target="_blank" class="relative flex items-center justify-center h-12">
                     <img class="object-contain" src="https://im.haberturk.com/assets/images/ramadan/ht_ramazan_eminevim_mobil.jpg" alt="" />
                 </a>
             </div>
             <div class="w-full" data-test="">
                 <div class="widget-dynamic-cover w-full h-16 flex justify-end items-center gap-5 relative bg-white" style="background-position: 0 0;background-image: url('https://im.haberturk.com/assets/laravel/images/common/ramadan/ramadan-banner-mobile.png'); background-size: cover; background-repeat: no-repeat;">
                     <div class="h-max w-full pl-12 pr-10">
-                        <a href="cbg://ramazan/anasayfa" rel="nofollow" target="_blank" class="block bg-[#D3A17B] font-bold text-white rounded-md py-1 text-center w-full text-sm px-0 desktop3xl:px-2">RAMAZAN SAYFAMIZ</a>
+                        <a href="cbg://ramazan/anasayfa" rel="nofollow" target="_blank" onclick="return true;" class="block bg-[#D3A17B] font-bold text-white rounded-md py-1 text-center w-full text-sm px-0 desktop3xl:px-2">RAMAZAN SAYFAMIZ</a>
                     </div>
                 </div>
-            </div>s
+            </div>
         </div>
     </section>
     <script src="/js/common/ramadan/imsakiye.js?id=010af861d1323ac6dafdc2122bbe870c"></script>
